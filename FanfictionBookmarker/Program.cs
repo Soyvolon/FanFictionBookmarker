09ded4f9265e97fcb6e6f87a06623203102f0ddd
Andrew Bounds <andybounds@gmail.com>
2020-09-18 10:44:37 -0400

    Added Regex for URL validation for the FanficModel

--- FanfictionBookmarker/Program.cs
+++ FanfictionBookmarker/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore;
@@ -10,12 +11,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Newtonsoft.Json;
+
 namespace FanfictionBookmarker
 {
     public class Program
     {
+        public static string[] ValidUrls;
+        public static string RegexUrls;
+
         public static void Main(string[] args)
         {
+            ReadConigs();
+
+            CalculateRegex();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -25,5 +35,48 @@ namespace FanfictionBookmarker
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void ReadConigs()
+        {
+            var root = "Configs";
+            var file = "urls.json";
+
+            if (!Directory.Exists(root))
+                Directory.CreateDirectory(root);
+
+            if (File.Exists(Path.Combine(root, file)))
+            {
+                using FileStream fs = new FileStream(Path.Combine(root, file), FileMode.Open);
+                using StreamReader sr = new StreamReader(fs);
+                var json = sr.ReadToEnd();
+
+                ValidUrls = JsonConvert.DeserializeObject<string[]>(json);
+            }
+            else
+            {
+                var data = new string[] { "" };
+                using var sw = new StreamWriter(new FileStream(Path.Combine(root, file), FileMode.Create));
+                var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+                foreach (string line in json.Split("\n"))
+                    sw.WriteLine(line);
+                sw.Close();
+
+                Console.WriteLine($"New URL configuration generated. Please open and edit {Path.Combine(root, file)}.");
+                Environment.Exit(0);
+            }
+        }
+
+        public static void CalculateRegex()
+        {
+            var regex = "";
+            foreach (var url in ValidUrls)
+            {
+                var data = url.Replace("www.", "");
+                regex += $"(?:www.)?{data}|";
+            }
+
+            RegexUrls = regex[0..(regex.Length - 1)];
+        }
     }
 }
